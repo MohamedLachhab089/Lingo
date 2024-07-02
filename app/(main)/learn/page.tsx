b6f26502f07eb01ef7ac70a63df2540c3206133e
Mohamed <mohamed.lachhab@emsi-edu.ma>
2024-07-02 18:49:00 +0100

    add db folder

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,27 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
+
 const LearnPage = () => {
-    return ( 
-        <div>
-            <h1>Learn Page</h1>
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress 
+            activeCourse={{title: "Spanish", imageSrc: "/images_lingo/es.svg"}}
+            hearts={5}
+            points={100}
+            hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+        <div className="h-[700px] bg-blue-400">
+
         </div>
-     );
-}
- 
+      </FeedWrapper>
+    </div>
+  );
+};
+
 export default LearnPage;
