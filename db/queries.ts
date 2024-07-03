f0ac315502d4be770d5bde9f7e088eb3091d595e
Mohamed <mohamed.lachhab@emsi-edu.ma>
2024-07-03 15:11:00 +0100

    update Units

--- db/queries.ts
+++ db/queries.ts
@@ -2,7 +2,7 @@ import { cache } from "react";
 import { auth } from "@clerk/nextjs/server";
 import db from "./drizzle";
 import { eq } from "drizzle-orm";
-import { courses, userProgress } from "./schema";
+import { challengeProgress, courses, units, userProgress } from "./schema";
 
 export const getUserProgress = cache(async () => {
   const { userId } = await auth();
@@ -17,6 +17,56 @@ export const getUserProgress = cache(async () => {
   });
 
   return data;
+});
+
+export const getUnits = cache(async () => {
+  const { userId } = await auth();
+  const userProgress = await getUserProgress();
+
+  if (!userId || !userProgress?.activeCourseId) {
+    return [];
+  }
+
+  const data = await db.query.units.findMany({
+    orderBy: (units, { asc }) => [asc(units.order)],
+    where: eq(units.courseId, userProgress.activeCourseId),
+    with: {
+      lessons: {
+        orderBy: (lessons, { asc }) => [asc(lessons.order)],
+        with: {
+          challenges: {
+            orderBy: (challenges, { asc }) => [asc(challenges.order)],
+            with: {
+              challengeProgress: {
+                where: eq(challengeProgress.userId, userId),
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+  const normalizedData = data.map((unit) => {
+    const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      // We wont publish any empty lessons but if they is its completed will be as false
+      if (lesson.challenges.length === 0) {
+        return { ...lesson, completed: false };
+      }
+
+      const allCompletedChallenges = lesson.challenges.every((challenge) => {
+        return (
+          challenge.challengeProgress &&
+          challenge.challengeProgress.length > 0 &&
+          challenge.challengeProgress.every((progress) => progress.completed)
+        );
+      });
+
+      return { ...lesson, completed: allCompletedChallenges };
+    });
+    return { ...unit, lessons: lessonsWithCompletedStatus };
+  });
+
+  return normalizedData;
 });
 
 export const getCourses = cache(async () => {
