646832bf894d7a267520d77118900ca216bbd2f9
Mohamed <mohamed.lachhab@emsi-edu.ma>
2024-07-03 02:23:59 +0100

    Done User Progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,23 +2,31 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-const LearnPage = () => {
+const LearnPage = async () => {
+  const userProgressData = await getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
-        <UserProgress 
-            activeCourse={{title: "Spanish", imageSrc: "/images_lingo/es.svg"}}
-            hearts={5}
-            points={100}
-            hasActiveSubscription={false}
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/images_lingo/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
         />
       </StickyWrapper>
       <FeedWrapper>
         <Header title="Spanish" />
-        <div className="h-[700px] bg-blue-400">
-
-        </div>
+        <div className="h-[700px]">hhhhhhhhhhhhhhhhhhhh</div>
       </FeedWrapper>
     </div>
   );
