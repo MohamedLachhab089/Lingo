6356291696c7d695702ef117776b82cc807fca66
Mohamed <mohamed.lachhab@emsi-edu.ma>
2024-07-04 18:39:16 +0100

    Fix challenge actions and FS

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -11,6 +11,9 @@ import { Challenge } from "./challenge";
 import { Footer } from "./footer";
 import { upsertChallengeProgress } from "@/actions/challenge-progress";
 import { reduceHearts } from "@/actions/user-progress";
+import { useAudio, useWindowSize } from "react-use";
+import { ResultCard } from "./result-card";
+import Confetti from "react-confetti";
 
 type Props = {
   initialPercentage: number;
@@ -34,7 +37,22 @@ export const Quiz = ({
   initialLessonChallenges,
   userSubscription,
 }: Props) => {
+  const { width, height } = useWindowSize();
+
+  const [finishAudio] = useAudio({
+    src: "/images_lingo/finish.mp3",
+    autoPlay: true,
+  });
+  const [correctAudio, _c, correctControls] = useAudio({
+    src: "/images_lingo/public_correct.wav",
+  });
+  const [incorrectAudio, _i, incorrectControls] = useAudio({
+    src: "/images_lingo/public_incorrect.wav",
+  });
   const [pending, startTransition] = useTransition();
+
+  const router = useRouter();
+  const [lessonId] = useState(initialLessonId);
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(initialPercentage);
   const [challenges] = useState(initialLessonChallenges);
@@ -90,7 +108,7 @@ export const Quiz = ({
                 console.error("Missing hearts");
                 return;
               }
-
+              correctControls.play();
               setStatus("correct");
               setPercentage((prev) => prev + 100 / challenges.length);
 
@@ -107,20 +125,66 @@ export const Quiz = ({
     } else {
       startTransition(() => {
         reduceHearts(challenge.id)
-        .then((response) => {
-          if (response?.error === "hearts") {
-            console.error("Missing hearts");
-            return;
-          }
+          .then((response) => {
+            if (response?.error === "hearts") {
+              console.error("Missing hearts");
+              return;
+            }
+            incorrectControls.play();
+            setStatus("wrong");
 
-          setStatus("wrong");
-
-          if (!response?.error) setHearts((prev) => Math.max(prev - 1, 0));
-        })
-        .catch(() => toast.error("Something went wrong. Please try again."));
+            if (!response?.error) setHearts((prev) => Math.max(prev - 1, 0));
+          })
+          .catch(() => toast.error("Something went wrong. Please try again."));
       });
     }
   };
+
+  if (!challenge) {
+    return (
+      <>
+        {finishAudio}
+        <Confetti // animation of a successful challenge
+          width={width}
+          height={height}
+          recycle={false}
+          numberOfPieces={500}
+          tweenDuration={10000}
+        />
+        <div className="flex flex-col gap-4 lg:gap-y-8 max-w-lg mx-auto text-center items-center justify-center h-full">
+          <Image
+            src="/images_lingo/finish.svg"
+            alt="Finish"
+            className="hidden lg:block"
+            height={100}
+            width={100}
+          />
+          <Image
+            src="/images_lingo/finish.svg"
+            alt="Finish"
+            className="block lg:hidden"
+            height={50}
+            width={50}
+          />
+          <h1 className="text-lg font-bold text-neutral-700 lg:text-3xl">
+            Great job! <br /> You&apos;ve completed the lesson.
+          </h1>
+          <div className="flex w-full items-center gap-x-4">
+            <ResultCard variant="points" value={challenges.length * 10} />
+            <ResultCard
+              variant="hearts"
+              value={userSubscription?.isActive ? Infinity : hearts}
+            />
+          </div>
+        </div>
+        <Footer
+          lessonId={lessonId}
+          status="completed"
+          onCheck={() => router.push("/learn")}
+        />
+      </>
+    );
+  }
 
   const title =
     challenge.type === "ASSIST"
@@ -129,6 +193,8 @@ export const Quiz = ({
 
   return (
     <>
+      {incorrectAudio}
+      {correctAudio}
       <Header
         hearts={hearts}
         percentage={percentage}
